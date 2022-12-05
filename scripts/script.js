a2eb66ac6d80708dcb5e07e9198984bea74893b2
SomeoneWithOptions <andrescastesol@gmail.com>
2022-12-05 17:20:24 -0500

    added contact menu and animations

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const contactLink = document.querySelector(".page-1__header__menu");
+const contactMenu = document.querySelector(".contact-menu");
+
+contactLink.addEventListener("click", function (e) {
+  contactMenu.classList.remove("contact-menu--inactive");
+  contactMenu.classList.add("contact-menu--active");
+  e.stopPropagation();
+});
+
+document.querySelector("body").addEventListener("click", function (e) {
+  if (contactMenu.classList.contains("contact-menu--active")) {
+    contactMenu.classList.remove("contact-menu--active");
+    contactMenu.classList.add("contact-menu--inactive");
+  }
+  console.log(e);
+});
