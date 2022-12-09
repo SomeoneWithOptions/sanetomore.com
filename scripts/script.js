9dc12ee9f2b67d1cfc304fc0398e5cc70015ba7c
SomeoneWithOptions <andrescastesol@gmail.com>
2022-12-08 22:04:10 -0500

    formatting changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,17 @@
 const contactLink = document.querySelector(".page-1__header__menu");
 const contactMenu = document.querySelector(".contact-menu");
 
-function toggleMenu()
-{
+function toggleMenu() {
   contactMenu.classList.toggle("contact-menu--active");
   contactMenu.classList.toggle("contact-menu--inactive");
 }
 
-contactLink.addEventListener("click", function (e)
-{
-  (contactMenu.classList.contains("contact-menu--inactive")) && toggleMenu();
+contactLink.addEventListener("click", function (e) {
+  contactMenu.classList.contains("contact-menu--inactive") && toggleMenu();
   contactMenu.classList.add("contact-menu--active");
   e.stopPropagation();
 });
 
-window.addEventListener("click", function (e)
-{
-  (contactMenu.classList.contains("contact-menu--active")) && toggleMenu();
-
+window.addEventListener("click", function (e) {
+  contactMenu.classList.contains("contact-menu--active") && toggleMenu();
 });
