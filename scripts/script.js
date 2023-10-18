60141f72112241e680689c4bdb4cf5d678975caa
SomeoneWithOptions <andrescastesol@gmail.com>
2023-10-17 20:31:57 -0500

    added linkedin link

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,1 @@
-const contactLink = document.querySelector(".page-1__header__menu");
-const contactMenu = document.querySelector(".contact-menu");
-
-function toggleMenu() {
-  contactMenu.classList.toggle("contact-menu--active");
-  contactMenu.classList.toggle("contact-menu--inactive");
-}
-
-contactLink.addEventListener("click", function (e) {
-  contactMenu.classList.contains("contact-menu--inactive") && toggleMenu();
-  contactMenu.classList.add("contact-menu--active");
-  e.stopPropagation();
-});
-
-window.addEventListener("click", function (e) {
-  contactMenu.classList.contains("contact-menu--active") && toggleMenu();
-});
+const contactLink=document.querySelector(".page-1__header__menu"),contactMenu=document.querySelector(".contact-menu");function toggleMenu(){contactMenu.classList.toggle("contact-menu--active"),contactMenu.classList.toggle("contact-menu--inactive")}contactLink.addEventListener("click",function(t){contactMenu.classList.contains("contact-menu--inactive")&&toggleMenu(),contactMenu.classList.add("contact-menu--active"),t.stopPropagation()}),window.addEventListener("click",function(t){contactMenu.classList.contains("contact-menu--active")&&toggleMenu()});
