5a1ac3337528355c03524c101abab60b6ce4dced
SomeoneWithOptions <andrescastesol@gmail.com>
2024-11-17 13:20:37 -0500

    unminify js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-const contactLink=document.querySelector(".page-1__header__menu"),contactMenu=document.querySelector(".contact-menu");function toggleMenu(){contactMenu.classList.toggle("contact-menu--active"),contactMenu.classList.toggle("contact-menu--inactive")}contactLink.addEventListener("click",function(t){contactMenu.classList.contains("contact-menu--inactive")&&toggleMenu(),contactMenu.classList.add("contact-menu--active"),t.stopPropagation()}),window.addEventListener("click",function(t){contactMenu.classList.contains("contact-menu--active")&&toggleMenu()});
+const contactLink = document.querySelector(".page-1__header__menu")
+const contactMenu = document.querySelector(".contact-menu");
+function toggleMenu() {
+    contactMenu.classList.toggle("contact-menu--active"), contactMenu.classList.toggle("contact-menu--inactive");
+}
+contactLink.addEventListener("click", function (t) {
+    contactMenu.classList.contains("contact-menu--inactive") && toggleMenu(), contactMenu.classList.add("contact-menu--active"), t.stopPropagation();
+})
+window.addEventListener("click", function (t) {
+    contactMenu.classList.contains("contact-menu--active") && toggleMenu();
+});
