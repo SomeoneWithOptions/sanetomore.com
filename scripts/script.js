bdcc8a015bc77607f9667adbc21dea6de2b77be1
Andres Castellanos <85639790+SomeoneWithOptions@users.noreply.github.com>
2025-10-26 17:25:38 -0500

    Fix contact menu initialization errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,28 @@
-
-
-const contactLink = document.querySelector(".page-1__header__menu")
+const contactLink = document.querySelector(".page-1__header__menu");
 const contactMenu = document.querySelector(".contact-menu");
-function toggleMenu() {
-    contactMenu.classList.toggle("contact-menu--active"), contactMenu.classList.toggle("contact-menu--inactive");
+
+if (contactLink && contactMenu) {
+    const toggleMenu = () => {
+        contactMenu.classList.toggle("contact-menu--active");
+        contactMenu.classList.toggle("contact-menu--inactive");
+    };
+
+    if (!contactMenu.classList.contains("contact-menu--inactive")) {
+        contactMenu.classList.add("contact-menu--inactive");
+    }
+
+    contactLink.addEventListener("click", (event) => {
+        if (contactMenu.classList.contains("contact-menu--inactive")) {
+            toggleMenu();
+        }
+
+        contactMenu.classList.add("contact-menu--active");
+        event.stopPropagation();
+    });
+
+    window.addEventListener("click", () => {
+        if (contactMenu.classList.contains("contact-menu--active")) {
+            toggleMenu();
+        }
+    });
 }
-contactLink.addEventListener("click", function (t) {
-    contactMenu.classList.contains("contact-menu--inactive") && toggleMenu(), contactMenu.classList.add("contact-menu--active"), t.stopPropagation();
-})
-window.addEventListener("click", function (t) {
-    contactMenu.classList.contains("contact-menu--active") && toggleMenu();
-});
